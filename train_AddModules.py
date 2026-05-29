2050f9e06f3c8225d8c195908c19be8ba1fdd97c
赵继源 <2331032123@qq.com>
2026-05-29 10:30:16 +0800

    修改.gitignore-V2

--- train_AddModules.py
+++ train_AddModules.py
@@ -44,11 +44,11 @@ class _DummyEMA:
 
 # --- 模型配置 ---
 # 模型 YAML 配置文件路径
-DEFAULT_MODEL_YAML = "ultralytics/cfg/models/26-RGBT/2026-05-19/yolo26-RGBT-midfusion-ICAFusion.yaml"
+DEFAULT_MODEL_YAML = "ultralytics/cfg/models/26-RGBT/2026-05-13/yolo26-RGBT-midfusion-Att_EMA.yaml"
 
 # --- 训练结果保存 ---
 DEFAULT_PROJECT = "runs/FLIR/26dual-test"
-DEFAULT_NAME = "yolo26s-RGBT-midfusion-ICAFusion"
+DEFAULT_NAME = "yolo26s-RGBT-midfusion-Att_EMA"
 
 # --- 数据集 ---
 DEFAULT_DATA = "ultralytics/cfg/datasets/flir.yaml"
